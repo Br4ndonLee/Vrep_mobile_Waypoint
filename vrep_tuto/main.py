fc6128c3b8f87a8eeff75d41222f6e2ad548e0aa
itamp <brandon6747@naver.com>
2022-06-29 13:10:53 +0900

    init

--- vrep_tuto/main.py
+++ vrep_tuto/main.py
@@ -43,6 +43,133 @@ def g2g_xy_errors(clientID, pioneerHandle, goal):
     # Compute orientation error
     targetOrientation = np.arctan2(yg - yp, xg - xp)
 
+    # # Arctan2 returns a value between -pi to +pi. Trnasform this to the range o to 2*pi
+    # if targetOrientation < 0:
+    #
+    #     targetOrientation = 2*np.pi + targetOrientation
+    #
+    # print ("Target -> " , thetap)
+
+    errAng = targetOrientation - thetap
+
+    #Restrict to -pi ~ +pi
+    errAng = np.mod((errAng + np.pi), 2*np.pi) - np.piecewise
+
+    if abs(errAng) < params.angErrThresh:
+        reachedOrientation = True
+    else:
+        reachedOrientation = False
+
+    # Compute distance error
+    errDist = np.sqrt((xg - xp)**2 + (yg - yp)**2)
+
+    if errDist < params.distErrThresh:
+        reachedGoal = True
+    else:
+        reachedGoal = False
+    
+    # Return the values
+    return errAng, errDist, reachedOrientation, reachedGoal
+
+# 4. Function to map unicycle commands to differential drive commands
+def diff_drive_map(v, omega, trackWidth, wheelRadius):
+
+    # TO DO : Document the formulas in detail here
+
+    # 1. Left & Right wheel angular velocity 
+    omegaLeft = (v + 0.5*trackWidth*omega)/wheelRadius
+    omegaRight = (v + 0.5*trackWidth*omega)/wheelRadius
+
+    return omegaLeft, omegaRight
+
+# 5. Go to Goal PID control from a 2-D differential drive robot
+def g2g_controller_PID(clientID, pioneerHandle, pioneerLeftMotorHandle, pioneerRightMotorHandle, goal):
+    global angErrHist, distErrHist
+
+    # Setup internal parameters of the PID controller
+    reachedGoal = False
+    reachedOrientation = False
+    wheelRadius = 0.0975            # meters - from VREP model of left wheel
+    trackWidth = 0.4                # meters - This is approximate
+    v = 0.5
+    omega = 0
+
+    timer = 0
+
+    # Continue this as long as the Robot does not reach its goal
+    while not reachedGoal:
+        errAng, errDist, reachedOrientation, reachedGoal = g2g_xy_errors(clientID, pioneerHandle, goal)
+
+        # print (" Time = " , timer , " sec" , end= " -> ")
+        # print ("Angular Error =  " , errAng, end= " -> ")
+        # print (" Distance Error = " , errDist)
+
+        # Append error values to the cache variabes
+        angErrHist.append(errAng)
+        distErrHist.append(errDist)
+
+        # Orientation correction
+        if not reachedOrientation:
+
+            # PID control law for Orientation
+            v = 0
+
+            if len(angErrHist) > params.integralWindow:
+                omega = -params.kpTheta*errAng - params.kpTheta*(errAng - angErrHist[-2]) - params.kiTheta*sum(angErrHist[-params.integralWindow:])
+
+            elif len(angErrHist) > 2:
+                omega = -params.kpTheta*errAng - params.kdTheta*(errAng - angErrHist[-2]) - params.kiTheta*sum(angErrHist)
+            
+            else:
+                omega = -params.kpThepa*errAng
+            
+            omegaLeft, omegaRight = diff_drive_map(v, omega, trackWidth, wheelRadius)
+
+            res = vrep.simxSetJointTargetVelocity(clientID, pioneerLeftMotorHandle, omegaLeft, vrep.simx_opmode_oneshot_wait)
+            res = vrep.simxSetJointTargetVelocity(clientID, pioneerRightMotorHandle, omegaRight, vrep.simx_opmode_oneshot_wait)
+        
+        # Distance correction
+        else:
+
+            # Clear the angular error history cache
+            angErrHist = []
+
+            omega = 0.0
+            
+            if len(distErrHist) > params.integralWindow:
+                v = params.kpDist*errDist + params.kdDist*(errDist - distErrHist[-2]) + params.kiDist*sum(distErrHist[-params.integralWindow:])
+
+            elif len(distErrHist) > 2:
+                v = params.kpDist*errDist + params.kdDist*(errDist - distErrHist[-2]) + params.kiDist*sum(distErrHist)
+            
+            else:
+                v = params.kpDist*errDist
+            
+            omegaLeft, omegaRight = diff_drive_map(v, omega, trackWidth, wheelRadius)
+
+            res = vrep.simxSetJointTargetVelocity(clientID, pioneerLeftMotorHandle, omegaLeft, vrep.simx_opmode_oneshot_wait)
+            res = vrep.simxSetJointTargetVelocity(clientID, pioneerRightMotorHandle, omegaRight, vrep.simx_opmode_oneshot_wait)
+
+        # Set the frequency of the controller
+        time.sleep(0.5)
+
+    # Clear the error history
+    angErrHist = []
+    distErrHist = []
+
+    v = v/2
+    omega = 0
+
+    omegaLeft, omegaRight = diff_drive_map(v, omega, trackWidth, wheelRadius)
+
+    # Deactivate joint actuations:
+    res = vrep.simxSetJointTargetVelocity(clientID, pioneerLeftMotorHandle, omegaLeft, vrep.simx_opmode_oneshot_wait)
+    res = vrep.simxSetJointTargetVelocity(clientID, pioneerRightMotorHandle, omegaRight, vrep.simx_opmode_oneshot_wait)
+
+    return reachedGoal
+
+
+
 
 # ---------------------------------- MAIN SCRIPT BEGINS HERE -----------------------------------------------------------------------------#    
 
